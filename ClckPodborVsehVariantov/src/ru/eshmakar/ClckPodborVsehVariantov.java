7f6fdb80c9d62525e8dd4c492b2168d0cb764186
hasanshaih <hafizov.marat@mail.ru>
2021-03-03 22:58:25 +0300

    был оптимизирован код

--- ClckPodborVsehVariantov/src/ru/eshmakar/ClckPodborVsehVariantov.java
+++ ClckPodborVsehVariantov/src/ru/eshmakar/ClckPodborVsehVariantov.java
@@ -1,6 +1,7 @@
 package ru.eshmakar;
 
 //почему-то после продолжител. времени выдает: java heap out of memory
+
 import java.io.InputStream;
 import java.net.URL;
 import java.util.Scanner;
@@ -14,60 +15,31 @@ public class ClckPodborVsehVariantov {
             if (propustit(nol)) continue;
             for (char nol2 = 'Y'; nol2 <= 'z'; nol2++) {
                 if (propustit(nol2)) continue;
-                for (char nol3 = 'A'; nol3 <= 'z'; nol3++) {
+                for (char nol3 = 'E'; nol3 <= 'z'; nol3++) {
                     if (propustit(nol3)) continue;
                     for (char nol4 = '0'; nol4 <= 'z'; nol4++) {
                         if (propustit(nol4)) continue;
                         for (char nol5 = '0'; nol5 <= 'z'; nol5++) {
                             if (propustit(nol5)) continue;
                             url = String.format("https://clck.ru/%c%c%c%c%c", nol, nol2, nol3, nol4, nol5);
-//                            System.out.println(url);
                             InputStream response = null;
                             try {
                                 response = new URL(url).openStream();
                                 Scanner scanner = new Scanner(response);
                                 String responseBody = scanner.useDelimiter("\\A").next();
                                 String titleFromSite = responseBody.substring(responseBody.indexOf("<title>") + 7, responseBody.indexOf("</title>"));
-                                if (!titleFromSite.equals("Google Документы ‒ бесплатно создавайте и редактируйте документы в Интернете.")
-                                        && !titleFromSite.equals("Google Таблицы ‒ бесплатно создавайте и редактируйте таблицы в Интернете.")
-                                        && !titleFromSite.contains("Яндекс.Карты") && !titleFromSite.equals("Пройди тест на денежное мышление")
-                                        && !titleFromSite.equals("Обратная связь - Группа медицинских компаний студия")
-                                        && !titleFromSite.equals("МТС-Банк") && !titleFromSite.equals("Index - MerchantNew")
-                                        && !titleFromSite.contains("РУСХИТ") && !titleFromSite.equals(" Google Карты ") && !titleFromSite.equals("Яндекс.Картинки")
-                                        && !titleFromSite.equals("Срочноденьги Заявка") && !titleFromSite.equals("Best dating worldwide")
-                                        && !titleFromSite.equals("Яндекс") && !titleFromSite.equals("МВД РОССИИ: УВЕДОМЛЕНИЕ О ШТРАФЕ")
-                                        && !titleFromSite.equals("Страница оплаты") && !titleFromSite.contains("html") && !titleFromSite.equals("New phoenix")
-                                        && !titleFromSite.equals("Корзина") && !titleFromSite.equals("Ru_kitchent at Taplink") && !titleFromSite.equals("Самолет")
-                                        && !titleFromSite.contains("Авито") && !titleFromSite.equals("Оценка курьера") && !titleFromSite.equals("Добро пожаловать!")
-                                        && !titleFromSite.contains("PeoplePass") && !titleFromSite.equals("axilog.ru - служба доставки")
-                                        && !titleFromSite.equals("QIWI") && !titleFromSite.equals("Расчёт стоимости полиса ОСАГО онлайн")
-                                        && !titleFromSite.contains("секс") && !titleFromSite.equals("Ваш заказ | Вилгуд")
-                                        && !titleFromSite.contains("mistery") && !titleFromSite.equals("Как самозанятым получать бесплатные смены  — Яндекс.Про")
-                                        && !titleFromSite.contains("xvideo") && !titleFromSite.contains("ЮMoney") && !titleFromSite.equals("Оплата")
-                                        && !titleFromSite.contains("porno") && !titleFromSite.equals("") && !titleFromSite.contains("Линзмастер")
-                                        && !titleFromSite.contains("Telegraph") && !titleFromSite.equals("Короткий URL для всех!")
-                                        && !titleFromSite.contains("Ташкент") && !titleFromSite.equals(" ") && !titleFromSite.contains("Redirecting...")
-                                        && !titleFromSite.equals("Get Laid Tonight") && !titleFromSite.equals("Ой!")
-                                        && !titleFromSite.equals("PravSistem.Club") && !titleFromSite.equals("Warning! | There might be a problem with the requested link")
-                                        && !titleFromSite.contains("Квартиры свободные") && !titleFromSite.equals("Статус заказа")) {
-                                    System.out.println(url + " - " + titleFromSite + "(№: " + count++ + ")");
-                                }
-//                                System.out.println(++pop + ". " + url + " - " + titleFromSite);
-                            } catch (Exception ex) {
+
+                                if (skryvat(titleFromSite))
+                                    System.out.printf("%d. %s - %s\n", ++count, url, titleFromSite);
+
+                            } catch (Exception ignored) {
                             } finally {
                                 try {
                                     assert response != null;
                                     response.close();
-                                } catch (Exception ex) {
+                                } catch (Exception ignored) {
                                 }
-//                            System.out.printf("%d. %c%c%c%c%c\n", count++, nol, nol2, nol3, nol4, nol5);
-                      /*      temp = String.format("%d. %c%c%c%c%c\n", count++, nol, nol2, nol3, nol4, nol5);
-                            if (temp.contains("TXR9S")) {
-                                System.out.print(temp);
-                                System.exit(0);
-                            }*/
                             }
-
                         }
                     }
                 }
@@ -76,10 +48,34 @@ public class ClckPodborVsehVariantov {
     }
 
     private static boolean propustit(char a) {
-        if (a == ':' || a == ';' || a == '<'
+        return a == ':' || a == ';' || a == '<'
                 || a == '=' || a == '>'
                 || a == '?' || a == '@' || a == '[' || a == '\\'
-                || a == ']' || a == '^' || a == '_' || a == '`') return true;
-        return false;
+                || a == ']' || a == '^' || a == '_' || a == '`';
+    }
+
+    private static boolean skryvat(String s) {
+        return !s.equals("Google Документы ‒ бесплатно создавайте и редактируйте документы в Интернете.")
+                && !s.equals("Google Таблицы ‒ бесплатно создавайте и редактируйте таблицы в Интернете.")
+                && !s.contains("Яндекс.Карты") && !s.equals("Пройди тест на денежное мышление")
+                && !s.equals("Обратная связь - Группа медицинских компаний студия")
+                && !s.equals("МТС-Банк") && !s.equals("Index - MerchantNew")
+                && !s.contains("РУСХИТ") && !s.equals(" Google Карты ") && !s.equals("Яндекс.Картинки")
+                && !s.equals("Срочноденьги Заявка") && !s.equals("Best dating worldwide")
+                && !s.equals("Яндекс") && !s.equals("МВД РОССИИ: УВЕДОМЛЕНИЕ О ШТРАФЕ")
+                && !s.equals("Страница оплаты") && !s.contains("html") && !s.equals("New phoenix")
+                && !s.equals("Корзина") && !s.equals("Ru_kitchent at Taplink") && !s.equals("Самолет")
+                && !s.contains("Авито") && !s.equals("Оценка курьера") && !s.equals("Добро пожаловать!")
+                && !s.contains("PeoplePass") && !s.equals("axilog.ru - служба доставки")
+                && !s.equals("QIWI") && !s.equals("Расчёт стоимости полиса ОСАГО онлайн")
+                && !s.contains("секс") && !s.equals("Ваш заказ | Вилгуд")
+                && !s.contains("mistery") && !s.equals("Как самозанятым получать бесплатные смены  — Яндекс.Про")
+                && !s.contains("xvideo") && !s.contains("ЮMoney") && !s.equals("Оплата")
+                && !s.contains("porno") && !s.equals("") && !s.contains("Линзмастер")
+                && !s.contains("Telegraph") && !s.equals("Короткий URL для всех!")
+                && !s.contains("Ташкент") && !s.equals(" ") && !s.contains("Redirecting...")
+                && !s.equals("Get Laid Tonight") && !s.equals("Ой!")
+                && !s.equals("PravSistem.Club") && !s.equals("Warning! | There might be a problem with the requested link")
+                && !s.contains("Квартиры свободные") && !s.equals("Статус заказа");
     }
 }
